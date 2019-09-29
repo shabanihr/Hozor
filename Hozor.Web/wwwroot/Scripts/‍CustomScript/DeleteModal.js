7e02f6b962251fa146bcb529f88db7c7095cf8b3
Hamid Shabani <shabani@irankavehsaipa.com>
2019-09-29 18:42:40 +0330

    Insert DeleteModal.js

---
+++ Hozor.Web/wwwroot/Scripts/‍CustomScript/DeleteModal.js
@@ -0,0 +1,23 @@
+﻿
+
+var idItem;
+function DeleteItem(id) {
+    idItem = id;
+    $("#myModal").modal();
+}
+
+function deleteOk() {
+    $.get("/Admin/Notifications/Delete/" + idItem,
+        function () {
+            $("#myModal").modal('hide');
+            $("#record_" + idItem).hide('slow');
+        });
+}
+
+
+$(document).ready(function () {
+
+    // create DatePicker from input HTML element
+    $("#endDate").kendoDatePicker();
+    $("#startDate").kendoDatePicker();
+});
