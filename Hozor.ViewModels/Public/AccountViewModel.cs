22fe9c8763cea98088cb76eb6de0f99a056fcad7
Hamid Shabani <shabani@irankavehsaipa.com>
2019-09-29 18:31:21 +0330

    Insert _DeleteModal.cshtml

--- Hozor.ViewModels/Public/AccountViewModel.cs
+++ Hozor.ViewModels/Public/AccountViewModel.cs
@@ -1,10 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Hozor.ViewModels.Public
 {
     class AccountViewModel
     {
+        public class RegisterViewModel
+        {
+            [Display(Name = "نام کاربری")]
+            [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+            public string UserName { get; set; }
+
+            [Display(Name = "کلمه عبور")]
+            [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            [Display(Name = "تکرار کلمه عبور")]
+            [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+            [DataType(DataType.Password)]
+            [Compare("Password", ErrorMessage = "کلمه های عبور مغایرت دارند")]
+            public string RePassword { get; set; }
+
+            [Display(Name = "فعال")]
+            public Boolean IsActive { get; set; }
+
+        }
     }
 }
